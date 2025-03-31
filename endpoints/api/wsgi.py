ab34d1e9eb046701f10150df6dba42c889f2b6eb
comphortine siwende <comphortine@kali.comsiwende@gmail.com>
2025-03-31 15:20:22 +0300

    create postgresql connectivity

--- endpoints/api/wsgi.py
+++ endpoints/api/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-settings_module = 'api.deployment' if 'WEBSITE_HOSTnAME' in os.environ else 'api.settings'
+settings_module = 'api.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'api.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
